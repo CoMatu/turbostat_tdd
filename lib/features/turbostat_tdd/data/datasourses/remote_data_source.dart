a35c531bd4d555713db12153fad524fc587c8013
CoMatu <30466272+CoMatu@users.noreply.github.com>
2019-10-29 22:20:25 +0500

    test firestore in progress

--- lib/features/turbostat_tdd/data/datasourses/remote_data_source.dart
+++ lib/features/turbostat_tdd/data/datasourses/remote_data_source.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart' as fs;
 import 'package:turbostat_tdd/features/turbostat_tdd/data/models/car_model.dart';
+import 'package:meta/meta.dart';
 
 abstract class TurbostatRemoteDataSource {
 
@@ -6,4 +8,23 @@ abstract class TurbostatRemoteDataSource {
 
   Future<List<CarModel>> getAllCarModels(String userId);
 
+}
+
+class TurbostatRemoteDataSourceImpl implements TurbostatRemoteDataSource {
+  final fs.Firestore firestore;
+
+  TurbostatRemoteDataSourceImpl({@required this.firestore});
+  @override
+  Future<List<CarModel>> getAllCarModels(String userId) async {
+    final res = firestore.collection(userId);
+    
+    return null;
+  }
+
+  @override
+  Future<CarModel> getConcreteCarModel(String carId) {
+    // TODO: implement getConcreteCarModel
+    return null;
+  }
+  
 }
--- test/features/data/datasourses/turbostat_remote_data_sourse_test.dart
+++ test/features/data/datasourses/turbostat_remote_data_sourse_test.dart
@@ -1,18 +1,52 @@
 import 'package:cloud_firestore/cloud_firestore.dart' as fs;
 import 'package:firebase_core/firebase_core.dart' as fb;
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:turbostat_tdd/features/turbostat_tdd/data/datasourses/remote_data_source.dart';
+
+class MockFirestore extends Mock implements fs.Firestore {}
 
 void main() {
   fb.FirebaseApp app;
   fs.Firestore firestore;
+  final String userId = '12345';
 
-  setUp(() async {
-    app = await fb.FirebaseApp.configure(
-      name: 'test',
-      options: const fb.FirebaseOptions(
-        googleAppID: 'com.example.turbostat_tdd',
-        projectID: 'carstat-d0da1'
-      )
+  setUpAll(() async {
+    // Required for FirebaseApp.configure
+    fb.FirebaseApp.channel.setMockMethodCallHandler(
+      (MethodCall methodCall) async {},
     );
+
+    app = await fb.FirebaseApp.configure(
+        name: 'testApp',
+        options: const fb.FirebaseOptions(
+          googleAppID: '1:1234567890:ios:42424242424242',
+          projectID: 'carstat-d0da1',
+          gcmSenderID: '1234567890',
+        ));
+
+    firestore = fs.Firestore(app: app);
+  });
+
+  test('app exists', () {
+    expect(app, isNotNull);
+    expect(firestore, isNotNull);
+  });
+
+  group('Collections and documents', () {
+    TurbostatRemoteDataSourceImpl remoteDataSource;
+    fs.CollectionReference ref;
+    setUp(() {
+      remoteDataSource = TurbostatRemoteDataSourceImpl(firestore: firestore);
+      ref = firestore.collection(userId);
+    });
+
+    test('should get all cars from firestore by userID', () async {
+      when(firestore.collection(any)).thenAnswer((_) => ref);
+      final result = await remoteDataSource.getAllCarModels(userId);
+
+      expect(result, ref);
+    });
   });
 }
