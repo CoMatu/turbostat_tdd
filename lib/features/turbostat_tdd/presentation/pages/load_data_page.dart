7a3db3c3e11bc5476907255fae4e33fd65080025
CoMatu <30466272+CoMatu@users.noreply.github.com>
2019-11-05 23:02:10 +0500

    load data page in progress

--- lib/features/turbostat_tdd/presentation/pages/load_data_page.dart
+++ lib/features/turbostat_tdd/presentation/pages/load_data_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:turbostat_tdd/features/turbostat_tdd/data/models/car_model.dart';
 import 'package:turbostat_tdd/features/turbostat_tdd/presentation/bloc/bloc.dart';
 import 'package:turbostat_tdd/features/turbostat_tdd/presentation/widgets/widgets.dart';
 import 'package:turbostat_tdd/injection_container.dart';
@@ -18,7 +19,7 @@ class LoadDataPage extends StatelessWidget {
       builder: (_) => sl<LoadDataBloc>(),
       child:
           BlocBuilder<LoadDataBloc, LoadDataState>(builder: (context, state) {
-            BlocProvider.of<LoadDataBloc>(context).add(GetAllCar());
+        BlocProvider.of<LoadDataBloc>(context).add(GetAllCar());
 //            state = Loading();
         if (state is InitialLoadDataState) {
           return Container(
@@ -44,8 +45,11 @@ class LoadDataPage extends StatelessWidget {
           return Column(
             children: <Widget>[
               Container(
+                alignment: Alignment.centerLeft,
                 height: 50,
-                child: new DropdownCarButton(),
+                child: DropdownCarButton(
+                  listAll: state.listAll,
+                ),
               ),
               Expanded(
                 child: Placeholder(),
@@ -59,13 +63,36 @@ class LoadDataPage extends StatelessWidget {
   }
 }
 
-class DropdownCarButton extends StatelessWidget {
+class DropdownCarButton extends StatefulWidget {
+  final List<CarModel> listAll;
   const DropdownCarButton({
+    @required this.listAll,
     Key key,
   }) : super(key: key);
 
   @override
+  _DropdownCarButtonState createState() => _DropdownCarButtonState(listAll);
+}
+
+class _DropdownCarButtonState extends State<DropdownCarButton> {
+  List<CarModel> listAll;
+  _DropdownCarButtonState(this.listAll);
+  get dropDownItems => listAll.map<DropdownMenuItem<Text>>((v) {
+    var text = v.carMark+' '+v.carModel +' '+v.carYear.toString();
+        return DropdownMenuItem<Text>(
+          child: Text(text),
+        );
+      }).toList();
+
+  @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(left: 12.0),
+      child: DropdownButton(
+        items: dropDownItems,
+        onChanged: (newValue) {
+        },
+      ),
+    );
   }
 }
