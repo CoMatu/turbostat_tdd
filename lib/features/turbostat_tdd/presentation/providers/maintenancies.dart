3a8a8adf1a0c80abec6e60f9c07a3bc8bdf42190
CoMatu <30466272+CoMatu@users.noreply.github.com>
2020-01-05 19:11:24 +0500

    сделано добавление операции ТО

--- lib/features/turbostat_tdd/presentation/providers/maintenancies.dart
+++ lib/features/turbostat_tdd/presentation/providers/maintenancies.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:turbostat_tdd/features/turbostat_tdd/data/models/maintenance_model.dart';
+import 'package:turbostat_tdd/features/turbostat_tdd/domain/usecases/add_maintenance_model.dart';
 import 'package:turbostat_tdd/injection_container.dart';
 
 class Maintenances extends ChangeNotifier {
@@ -11,6 +12,8 @@ class Maintenances extends ChangeNotifier {
       UnmodifiableListView(_maintenances);
   
   void add(MaintenanceModel model, String carId) async {
-    await sl().addMaintenanceModel(model, carId);
+    await sl<AddMaintenanceModel>().addMaintenanceModel(model, carId);
+    _maintenances.add(model);
+    notifyListeners();
   }
 }
