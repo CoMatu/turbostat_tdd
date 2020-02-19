6184678541bfc28db45f39f174f68fd09397ac03
CoMatu <matusevichvv@gmail.com>
2020-02-19 11:59:29 +0500

    detail page in progress

--- lib/features/turbostat_tdd/presentation/pages/maintenance_details_page.dart
+++ lib/features/turbostat_tdd/presentation/pages/maintenance_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:turbostat_tdd/features/turbostat_tdd/data/models/entry_model.dart';
+import 'package:turbostat_tdd/features/turbostat_tdd/data/models/maintenance_model.dart';
 import 'package:turbostat_tdd/features/turbostat_tdd/presentation/providers/providers.dart';
 import 'package:turbostat_tdd/features/turbostat_tdd/presentation/widgets/widgets.dart';
 
@@ -11,13 +13,25 @@ class MaintenanceDetailsPage extends StatefulWidget {
 }
 
 class _MaintenanceDetailsPageState extends State<MaintenanceDetailsPage> {
+  double totalPrice;
+  EntryModel model;
+  MaintenanceModel _maintenanceModel;
 
   @override
   void initState() {
+    totalPrice = Provider.of<PartsCart>(context, listen: false).totalPrice;
+    final _listMaintenancies =
+        Provider.of<Maintenances>(context, listen: false).maintenances;
 
-    
     super.initState();
   }
+
+  @override
+  void didChangeDependencies() {
+    model = ModalRoute.of(context).settings.arguments;
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -42,42 +56,82 @@ class _MaintenanceDetailsPageState extends State<MaintenanceDetailsPage> {
             Divider(),
             Padding(
               padding: const EdgeInsets.all(14.0),
-              child: Text('До ближайшей операции ТО осталось ХХХ дней или 00000 км'),
+              child: Text(
+                  'До ближайшей операции ТО осталось ХХХ дней или 00000 км'),
             ),
             Text('Set reminder'),
             Divider(),
-            Text('Список запасных частей:'),
-            Consumer<Parts>(
-                          builder: (context, partsList, child) {
-                            return Padding(
-                              padding: const EdgeInsets.only(
-                                left: 12.0,
-                              ),
-                              child: Column(
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0, right: 14.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Text('Список запасных частей:'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                  Consumer<PartsCart>(
+                    builder: (context, partsList, child) {
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                          left: 12.0,
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            // TODO добавить 18
+                            ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: partsList.partsCart.length,
+                              itemBuilder: (BuildContext context, int index) =>
+                                  Row(
                                 children: <Widget>[
-                                  // TODO добавить 18
-                                  ListView.builder(
-                                    shrinkWrap: true,
-                                    itemCount: partsList.parts.length,
-                                    itemBuilder:
-                                        (BuildContext context, int index) =>
-                                            Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                            child: Text(partsList
-                                                .parts[index].partName)),
-                                        Text(partsList.parts[index].partPrice
-                                            .toString()),
-                                        //  Text(numberOfPart.toString()),
-                                      ],
-                                    ),
-                                  ),
+                                  Expanded(
+                                      child: Text(
+                                          partsList.partsCart[index].partName)),
+                                  Text(partsList.partsCart[index].partPrice
+                                      .toString()),
+                                  //  Text(numberOfPart.toString()),
                                 ],
                               ),
-                            );
-                          },
-                        )
-
+                            ),
+                            Divider(),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                    child:
+                                        Text('Amount: ')), // TODO add to i18n
+                                Text(totalPrice.toString()),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text('Стоимость выполнения работ: '),
+                      ),
+                      Text(model.entryWorkPrice.toString()),
+                    ],
+                  ),
+                  Container(
+                    height: 10.0,
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text('Notes: '),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(model.entryNote),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
