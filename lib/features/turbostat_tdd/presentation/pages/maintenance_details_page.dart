4d5772ed139d138b211f9b560415beab6f1fea9d
CoMatu <30466272+CoMatu@users.noreply.github.com>
2020-02-23 16:14:20 +0500

    ddd

--- lib/features/turbostat_tdd/presentation/pages/maintenance_details_page.dart
+++ lib/features/turbostat_tdd/presentation/pages/maintenance_details_page.dart
@@ -11,13 +11,17 @@ class MaintenanceDetailsPage extends StatefulWidget {
 }
 
 class _MaintenanceDetailsPageState extends State<MaintenanceDetailsPage> {
+  double totalPrice = 0;
 
   @override
   void initState() {
-
-    
+    final partsList = Provider.of<Parts>(context, listen: false).parts;
+    for(int i = 0; i < partsList.length; i++) {
+       totalPrice = partsList[i].partPrice + totalPrice;
+    }
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -42,42 +46,80 @@ class _MaintenanceDetailsPageState extends State<MaintenanceDetailsPage> {
             Divider(),
             Padding(
               padding: const EdgeInsets.all(14.0),
-              child: Text('До ближайшей операции ТО осталось ХХХ дней или 00000 км'),
+              child: Text(
+                  'До ближайшей операции ТО осталось ХХХ дней или 00000 км'),
             ),
-            Text('Set reminder'),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0, right: 14.0),
+              child: Text('Set reminder'),
+            ),
             Divider(),
-            Text('Список запасных частей:'),
-            Consumer<Parts>(
-                          builder: (context, partsList, child) {
-                            return Padding(
-                              padding: const EdgeInsets.only(
-                                left: 12.0,
-                              ),
-                              child: Column(
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0, right: 14.0),
+              child: Text('Список запасных частей:'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0, right: 14.0),
+              child: Consumer<Parts>(
+                builder: (context, partsList, child) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      left: 12.0,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        // TODO добавить 18
+                        ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: partsList.parts.length,
+                          itemBuilder: (BuildContext context, int index) => Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: Column(
                                 children: <Widget>[
-                                  // TODO добавить 18
-                                  ListView.builder(
-                                    shrinkWrap: true,
-                                    itemCount: partsList.parts.length,
-                                    itemBuilder:
-                                        (BuildContext context, int index) =>
-                                            Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                            child: Text(partsList
-                                                .parts[index].partName)),
-                                        Text(partsList.parts[index].partPrice
-                                            .toString()),
-                                        //  Text(numberOfPart.toString()),
-                                      ],
-                                    ),
+                                  Container(
+                                    alignment: Alignment.centerLeft,
+                                    child:
+                                        Text(partsList.parts[index].partName),
                                   ),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        'Part code: ',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .overline,
+                                      ),
+                                      Text(
+                                        partsList.parts[index].partCode,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .overline,
+                                      ),
+                                    ],
+                                  )
                                 ],
-                              ),
-                            );
-                          },
-                        )
-
+                              )),
+                              Text(partsList.parts[index].partPrice.toString()),
+                              //  Text(numberOfPart.toString()),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0, right: 14.0),
+              child: Row(
+                children: <Widget>[
+                  Text('Total amount: '),
+                  Text(totalPrice.toString()),
+                ],
+              ),
+            ),
           ],
         ),
       ),
