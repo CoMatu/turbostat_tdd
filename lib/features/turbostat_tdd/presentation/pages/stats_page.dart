7277b59f585484f8fc5041685e268a997d2944b8
CoMatu <30466272+CoMatu@users.noreply.github.com>
2020-03-15 11:19:15 +0500

    добавил чарт статист с левыми данными

--- lib/features/turbostat_tdd/presentation/pages/stats_page.dart
+++ lib/features/turbostat_tdd/presentation/pages/stats_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:turbostat_tdd/features/turbostat_tdd/presentation/widgets/donut_pie_chart.dart';
 
 class StatsPage extends StatelessWidget {
   const StatsPage({Key key}) : super(key: key);
@@ -6,7 +7,62 @@ class StatsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Placeholder(color: Colors.red,),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Статистика расходов',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Wrap(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 6.0, right: 6.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () {},
+                    child: Text('В этом месяце'),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                      side: BorderSide(color: Colors.blueAccent),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 6.0, right: 6.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () {},
+                    child: Text('В прошлом месяце'),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                      side: BorderSide(color: Colors.blueAccent),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 6.0, right: 6.0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    onPressed: () {},
+                    child: Text('С начала года'),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                      side: BorderSide(color: Colors.blueAccent),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              child: Expanded(
+                child: DonutPieChart.withSampleData(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
