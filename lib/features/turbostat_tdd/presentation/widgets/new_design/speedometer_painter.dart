90c4d23e06dd2ae0a89f49ee1f3ecf6649520582
Matusevich <matu1@mail.ru>
2020-09-24 11:37:07 +0500

    шакала спидометра +

--- lib/features/turbostat_tdd/presentation/widgets/new_design/speedometer_painter.dart
+++ lib/features/turbostat_tdd/presentation/widgets/new_design/speedometer_painter.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
+import 'package:path_drawing/path_drawing.dart';
+
 class SpeedometerPainter extends StatelessWidget {
   const SpeedometerPainter({Key key}) : super(key: key);
 
@@ -8,16 +10,16 @@ class SpeedometerPainter extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        Container(
-          height: 140,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/rainbow_tableau.png'))),
+        Center(
+          child: CustomPaint(
+            painter: IndicatorBackgroundPainter(),
+            size: Size(265.0, 150.0),
+          ),
         ),
         Center(
           child: CustomPaint(
             painter: ShapePainter(),
-            size: Size(265.0, 140.0),
+            size: Size(265.0, 150.0),
           ),
         ),
       ],
@@ -38,15 +40,78 @@ class ShapePainter extends CustomPainter {
       stops: const [0.25, 0.5, 0.75, 1.0],
     );
 
-    final rect = Rect.fromLTRB(0, 20, 265, 265);
+    final rect = Rect.fromCenter(
+        center: Offset(size.width / 2, size.height),
+        height: size.width,
+        width: size.width);
     final startAngle = -math.pi;
-    final sweepAngle = math.pi;
+    final sweepAngle = math.pi / 1.8;
     final useCenter = false;
     final paint = Paint()
       ..shader = gradient.createShader(rect)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 10;
     canvas.drawArc(rect, startAngle, sweepAngle, useCenter, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class IndicatorBackgroundPainter extends CustomPainter {
+  @override
+  @override
+  void paint(Canvas canvas, Size size) {
+    // a fancy rainbow gradient
+    final rect = Rect.fromCenter(
+        center: Offset(size.width / 2, size.height),
+        height: size.width,
+        width: size.width);
+    final startAngle = -math.pi;
+    print(startAngle);
+    final sweepAngle = math.pi;
+    final useCenter = false;
+    final paint = Paint()
+      ..color = Color(0xFF24323F)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10;
+    canvas.drawArc(rect, startAngle, sweepAngle, useCenter, paint);
+
+    final scaleRect = Rect.fromCenter(
+        center: Offset(size.width / 2, size.height - 5),
+        height: size.width * 0.9 - 10,
+        width: size.width * 0.9);
+    double startAngleScale = -math.pi;
+    final sweepAngleScale = 0.01;
+    final scale = Paint()
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5;
+
+    for (int i = 0; i < 60; i++) {
+      canvas.drawArc(
+          scaleRect, startAngleScale, sweepAngleScale, useCenter, scale);
+      startAngleScale = startAngleScale + (2 * math.pi / 120);
+    }
+
+    final scaleRect2 = Rect.fromCenter(
+        center: Offset(size.width / 2, size.height - 5),
+        height: size.width * 0.9 - 15,
+        width: size.width * 0.9 - 5);
+    double startAngleScale2 = -math.pi;
+    final sweepAngleScale2 = 0.02;
+    final scale2 = Paint()
+      ..color = Colors.white70
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10;
+
+    for (int i = 0; i < 4; i++) {
+      canvas.drawArc(
+          scaleRect2, startAngleScale2, sweepAngleScale2, useCenter, scale2);
+      startAngleScale2 = startAngleScale2 + 2 * math.pi / 6;
+    }
   }
 
   @override
