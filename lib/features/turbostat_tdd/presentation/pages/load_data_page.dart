f16829f0886252ae5be31f365884830fa086ca48
CoMatu <30466272+CoMatu@users.noreply.github.com>
2020-09-25 21:43:30 +0500

    нижний бар на главной готов

--- lib/features/turbostat_tdd/presentation/pages/load_data_page.dart
+++ lib/features/turbostat_tdd/presentation/pages/load_data_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:turbostat_tdd/features/turbostat_tdd/presentation/bloc/bloc.dart';
 import 'package:turbostat_tdd/features/turbostat_tdd/presentation/providers/providers.dart';
+import 'package:turbostat_tdd/features/turbostat_tdd/presentation/widgets/new_design/custom_icon_widget.dart';
 import 'package:turbostat_tdd/features/turbostat_tdd/presentation/widgets/widgets.dart';
 import 'package:turbostat_tdd/generated/i18n.dart';
 import 'package:turbostat_tdd/injection_container.dart';
@@ -93,61 +95,64 @@ class LoadDataPage extends StatelessWidget {
 
   modalBottomSheet(context) {
     showModalBottomSheet(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        )),
         context: context,
         builder: (BuildContext ctx) {
-          return Container(
-            color: Color(0xFF737373),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Theme.of(context).canvasColor,
-                  borderRadius: BorderRadius.only(
-                      topLeft: const Radius.circular(20),
-                      topRight: const Radius.circular(20))),
-              child: Wrap(
-                children: <Widget>[
-                  Container(
-                    height: 20.0,
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  height: 20.0,
+                ),
+                ListTile(
+                  title: Text(S.of(context).add_maintenance_operation),
+                  leading: CustomIconWidget(
+                    fontAwesomeIcons: FontAwesomeIcons.wrench,
+                    borderColor: Color(0xFFF2994A),
+                    centerColor: Color(0xFFF2994A),
+                    iconColor: Color(0xFFF2C94C),
+                    iconSize: 14,
                   ),
-                  ListTile(
-                    title: Text(S.of(context).add_maintenance_regular),
-                    leading: Icon(Icons.calendar_today),
-                    onTap: () {
-                      Navigator.popAndPushNamed(context, 'add_maintenance');
-                    },
+                  onTap: () {
+                    final carId =
+                        Provider.of<CurrentCar>(context, listen: false)
+                            .currentCar
+                            .carId;
+                    Provider.of<MileageProvider>(context, listen: false)
+                        .getLastMileage(carId);
+                    Provider.of<PartsCart>(context, listen: false).clearCart();
+                    Provider.of<Parts>(context, listen: false).update(carId);
+                    Navigator.popAndPushNamed(context, 'add_entry');
+                  },
+                ),
+                ListTile(
+                  title: Text('Записать покупку запчастей / расходников'),
+                  leading: CustomIconWidget(
+                    fontAwesomeIcons: FontAwesomeIcons.shoppingBasket,
+                    borderColor: Color(0xFF2D9CDB),
+                    centerColor: Color(0xFF2D9CDB),
+                    iconColor: Color(0xFF56CCF2),
+                    iconSize: 14,
                   ),
-                  ListTile(
-                    title: Text(S.of(context).add_maintenance_operation),
-                    leading: Icon(Icons.build),
-                    onTap: () {
-                      final carId =
-                          Provider.of<CurrentCar>(context, listen: false)
-                              .currentCar
-                              .carId;
-                      Provider.of<MileageProvider>(context, listen: false)
-                          .getLastMileage(carId);
-                      Provider.of<PartsCart>(context, listen: false)
-                          .clearCart();
-                      Provider.of<Parts>(context, listen: false).update(carId);
-                      Navigator.popAndPushNamed(context, 'add_entry');
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Записать покупку запчастей / расходников'),
-                    leading: Icon(Icons.add_shopping_cart),
-                    onTap: () {
-                      Navigator.popAndPushNamed(context, 'add_part');
-                    },
-                  ),
+                  onTap: () {
+                    Navigator.popAndPushNamed(context, 'add_part');
+                  },
+                ),
 /*                   ListTile(
-                    title: Text(S.of(context).button_cancel),
-                    leading: Icon(Icons.subdirectory_arrow_left),
-                    onTap: () => Navigator.pop(context),
-                  ), */
-                  Container(
-                    height: 10.0,
-                  )
-                ],
-              ),
+                  title: Text(S.of(context).button_cancel),
+                  leading: Icon(Icons.subdirectory_arrow_left),
+                  onTap: () => Navigator.pop(context),
+                ), */
+                Container(
+                  height: 10.0,
+                )
+              ],
             ),
           );
         });
